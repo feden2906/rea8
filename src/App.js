fd8940fbfd454de7ba3fb2dcfc404646f2d61d25
Sergey23131 <semno321@gmail.com>
2021-08-29 14:22:56 +0300

    react8

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
 import {getUsers} from "./services/user.api";
 
-
+//
 export default function App() {
     let state = useSelector(state => state);
     let dispatch = useDispatch();
